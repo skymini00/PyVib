e45a205abaa041a0b0cce7059b374d390e61cc57
jso111 <joghalai@stanford.edu>
2015-11-01 18:04:40 -0800

    JSOraw

--- src/testHilbert.py
+++ src/testHilbert.py
@@ -11,16 +11,23 @@ import matplotlib.pyplot as plt
 import scipy.signal
 
 f=6000
+fs=100000
 
-t=np.arange(0,0.1,1/100000)
+t=np.arange(0,0.1,1/fs)
 x=np.sin(2*np.pi*f*t)
 
 x_h=scipy.signal.hilbert(x)
 
 plt.figure(1)
-plt.subplot(3,1,1)
+plt.subplot(4,1,1)
 plt.plot(t,x,'b')
-plt.subplot(3,1,2)
+plt.subplot(4,1,2)
 plt.plot(t,np.abs(x_h))
-plt.subplot(3,1,3)
+plt.subplot(4,1,3)
 plt.plot(t,np.unwrap(np.angle(x_h)))
+
+instfreq = fs/(2*np.pi)*np.diff(np.unwrap(np.angle(x_h)))
+#plt.subplot(4,1,4)
+#plt.figure(2)
+#plt.plot(instfreq)
+print(instfreq[0:20])
